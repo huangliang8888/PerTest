1a2e1546bf4b1b89c230805898573b0fbbce2f2b
yd <huangliang1919@gmail.com>
2023-02-06 15:32:54 +0800

    first commit

---
+++ apps/movie/case/base_case.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# _*_ coding:utf-8 _*_
+
+from apps.movie._movie_assert import MovieAssert
+
+
+class BaseCase(MovieAssert):
+    """
+    影院的基本用例
+    """
+
+    pass
---
+++ apps/movie/case/test_movie_001.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# _*_ coding:utf-8 _*_
+
+from apps.movie.case.base_case import BaseCase
+
+
+class TestMovie(BaseCase):
+    """
+    影院的用例
+    """
+
+    def test_movie_001(self):
+        """影院用例001：打开本地视频文件"""
+
+        pass
